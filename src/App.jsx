d9be28a698a1075dfa19d99b05b38aeec636324e
Busa Balázs <busa.balazs@intra.echotv.hu>
2025-03-31 15:35:58 +0200

    create the sections and browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+import { BrowserRouter } from "react-router-dom";
+
+import { Contact, Footer, Header, Hero, Services } from "./components";
 
 //------------------------------------------------------
 //------------------------------------------------------
 function App() {
-  
-
   return (
-    <>
-   <h1>Portfolio soon</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <Contact />
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 //------------------------------------------------------
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+import Services from "./Services";
+
+export { Contact, Footer, Header, Hero, Services };
