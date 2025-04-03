e9a16626c4ecb7eff9c3d28d6c8ae2a8bc759a8a
Busa Balázs <busa.balazs@intra.echotv.hu>
2025-04-03 12:12:58 +0200

    add html to canvas

--- src/components/LaptopCanvas.jsx
+++ src/components/LaptopCanvas.jsx
@@ -1,23 +1,30 @@
-import { Suspense, useEffect, useState } from "react";
-import { Canvas, extend } from "@react-three/fiber";
+import { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF, Html } from "@react-three/drei";
 
 //---------------------------------------------
 //---------------------------------------------
 const Laptop = () => {
+  const ref = useRef();
+
   const laptop = useGLTF("./assets/scene.gltf");
-  const testDiv = <div className="hero-div"></div>;
+
   return (
     <>
-      <mesh>
-        <hemisphereLight intensity={0.15} groundColor="black" />
-          <primitive object={laptop.scene} scale={3} />
-        <group>
-         
-            <Html   transform>
-              <div className="hero-div"></div>
-            </Html>
-     
+      <mesh ref={ref} >
+        <hemisphereLight intensity={2} groundColor="black" />
+        <group rotation={[0, -4.71, .3]}>
+          <primitive object={laptop.scene} scale={12} />
+        </group>
+
+        <group position={[0, 1.5, -0.5]}>
+          <Html transform >
+            <div className="hero-div">
+              <p>hello</p>
+              <p>hello</p>
+              <p>hello</p>
+            </div>
+          </Html>
         </group>
       </mesh>
     </>
@@ -28,12 +35,13 @@ const LaptopCanvas = () => {
   return (
     <>
       <Canvas
+        style={{ height: "800px" }}
         shadows
-        camera={{ position: [1, 1, 1], fov: 25 }}
         gl={{ preserveDrawingBuffer: true }}
+        camera={{ position: [0, 0, 5] }}
       >
         <Laptop />
-        <OrbitControls enableZoom={false} enableDamping={true} />
+        <OrbitControls enableZoom={false} />
       </Canvas>
     </>
   );
